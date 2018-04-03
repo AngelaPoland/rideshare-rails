987cb7fcf63a3ee7c2f8ce52a15d1ff4dfda5b4b
AngelaPoland <angelalpoland@gmail.com>
2018-04-03 11:52:37 -0700

    created controllers for trips, drivers, passengers, edited routes using resources, started making model for trips

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,40 @@
 Rails.application.routes.draw do
+  resources :trips
+  resources :drivers
+  resources :passengers
+
+
+  root 'trips#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+# 
+# Prefix Verb   URI Pattern                    Controller#Action
+#         trips GET    /trips(.:format)               trips#index
+#               POST   /trips(.:format)               trips#create
+#      new_trip GET    /trips/new(.:format)           trips#new
+#     edit_trip GET    /trips/:id/edit(.:format)      trips#edit
+#          trip GET    /trips/:id(.:format)           trips#show
+#               PATCH  /trips/:id(.:format)           trips#update
+#               PUT    /trips/:id(.:format)           trips#update
+#               DELETE /trips/:id(.:format)           trips#destroy
+
+#       drivers GET    /drivers(.:format)             drivers#index
+#               POST   /drivers(.:format)             drivers#create
+#    new_driver GET    /drivers/new(.:format)         drivers#new
+#   edit_driver GET    /drivers/:id/edit(.:format)    drivers#edit
+#        driver GET    /drivers/:id(.:format)         drivers#show
+#               PATCH  /drivers/:id(.:format)         drivers#update
+#               PUT    /drivers/:id(.:format)         drivers#update
+#               DELETE /drivers/:id(.:format)         drivers#destroy
+
+#    passengers GET    /passengers(.:format)          passengers#index
+#               POST   /passengers(.:format)          passengers#create
+# new_passenger GET    /passengers/new(.:format)      passengers#new
+# edit_passenger GET    /passengers/:id/edit(.:format) passengers#edit
+#     passenger GET    /passengers/:id(.:format)      passengers#show
+#               PATCH  /passengers/:id(.:format)      passengers#update
+#               PUT    /passengers/:id(.:format)      passengers#update
+#               DELETE /passengers/:id(.:format)      passengers#destroy
+#          root GET    /                              trips#index
