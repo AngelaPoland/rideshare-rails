463f4b585df22060c3b6578355aab7fe7502455e
Abiaina <abinnet.a@gmail.com>
2018-04-06 15:59:08 -0700

    commented out rating validation created

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,8 @@
 class Trip < ApplicationRecord
   belongs_to :driver
   belongs_to :passenger
+
+  # validates :rating, numericality: { only_integer: true }
+  # validates :rating, :inclusion => 0..5
+
 end
